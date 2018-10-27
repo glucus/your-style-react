ddb7c3be399939a0ebf655e60b792cad6f60c53b
Alexandra <voskresenskayaas@gmail.com>
2018-10-27 14:28:52 +0300

    added typechecking

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.scss';
 
-const Card = (props) => {
-
+const Card = props => {
   const { name, image, description, category } = props;
 
   return (
     <React.Fragment>
       <div className="card">
-        <img className="thumbnail" src={image} alt='' />
+        <img className="thumbnail" src={image} alt="" />
         <h4>{name}</h4>
         <div>{description}</div>
       </div>
     </React.Fragment>
   );
+};
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  image: PropTypes.string.isRequired
 };
 
 export default Card;
