ad472935ce3dc02c4a341723d32f96eda28156d0
Alexandra <voskresenskayaas@gmail.com>
2018-10-27 14:59:47 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Gallery from '../src/components/Gallery';
 import { pants } from './data';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-          <Gallery galleryItems={pants} />
+        <Gallery galleryItems={pants} />
       </div>
     );
   }
