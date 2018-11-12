2251e7b07dd352ca156f79f05aa6a38b2af82762
Alexandra <voskresenskayaas@gmail.com>
2018-11-12 15:36:06 +0300

    moved add new item handler to FOrm

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Form.scss';
 import { throws } from 'assert';
+import { Consumer } from '../../context';
 
 class Form extends React.Component {
 
@@ -27,36 +28,58 @@ class Form extends React.Component {
     })
   };
 
+  handleSubmit = (name, description, image, id, dispatch) => {
+
+    const newItem = {name, description, image, id};
+
+    dispatch ({
+        type: 'ADD_NEW_ITEM',
+        payload: newItem
+    })
+}
+
 
   render () {
 
-    const fieldsArr = Object.keys(this.state); // Object.getOwnPropertyNames();
-    // console.log(fieldsArr);
-
-    const updatedName = this.state.name || this.props.name;
-    const updatedDescription = this.state.description || this.props.description;
-    const updatedImage = this.state.image || this.props.image;
-
     return (
-      <form>
-        {fieldsArr.map (
-          item => <div className="form-row" key={item}>
-                    <label className="form-label">{item}</label>
-                    <input type='text'
-                           name={item}
-                           value={this.state[item]}
-                           onChange={this.handleChange}
-                           placeholder={this.props[item]}
-                    />
+        <Consumer>
+          {
+            value => {
+            
+              const fieldsArr = Object.keys(this.state); // Object.getOwnPropertyNames();
+            // console.log(fieldsArr);
+
+            const { dispatch } = value;
+
+              const updatedName = this.state.name || this.props.name;
+              const updatedDescription = this.state.description || this.props.description;
+              const updatedImage = this.state.image || this.props.image;
+              const id = this.props.id;
+
+              return (
+                <form>
+                  {fieldsArr.map (
+                    item => <div className="form-row" key={item}>
+                              <label className="form-label">{item}</label>
+                              <input type='text'
+                                    name={item}
+                                    value={this.state[item]}
+                                    onChange={this.handleChange}
+                                    placeholder={this.props[item]}
+                              />
+                            </div>
+                  )}
+                  <div className="icons-bottom">
+                    <i className="fas fa-check" onClick={
+                      this.handleSubmit.bind(this, updatedName, updatedDescription, updatedImage, id, dispatch)
+                    } />
                   </div>
-        )}
-        <div className="icons-bottom">
-          <i className="fas fa-check" onClick={
-            this.props.handleSubmit.bind(this, updatedName, updatedDescription, updatedImage)
-          } />
-        </div>
-      </form>
-    );
+                </form>
+              );
+            }
+          }
+        </Consumer>
+    )
   }
 }
 
